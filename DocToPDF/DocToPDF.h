52dbe5cc4b10b87b7ac9d908a179f8cdef37eeae
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2025-03-26 18:27:33 +0300

    Module created

---
+++ DocToPDF/DocToPDF.h
@@ -0,0 +1,18 @@
+//
+//  DocToPDF.h
+//  DocToPDF
+//
+//  Created by Roman Litvinovich on 26/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocToPDF.
+FOUNDATION_EXPORT double DocToPDFVersionNumber;
+
+//! Project version string for DocToPDF.
+FOUNDATION_EXPORT const unsigned char DocToPDFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocToPDF/PublicHeader.h>
+
+
